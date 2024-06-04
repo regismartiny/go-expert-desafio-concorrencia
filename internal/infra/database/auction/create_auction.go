cc817d0ad44675d986c921dd998307e8398c5a1c
Régis Martiny <regismartiny@gmail.com>
2024-06-04 23:08:14 +0000

    Implementação atualização status leilões expirados

--- internal/infra/database/auction/create_auction.go
+++ internal/infra/database/auction/create_auction.go
@@ -2,10 +2,14 @@ package auction
 
 import (
 	"context"
+	"fmt"
 	"fullcycle-auction_go/configuration/logger"
 	"fullcycle-auction_go/internal/entity/auction_entity"
 	"fullcycle-auction_go/internal/internal_error"
+	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,18 +23,25 @@ type AuctionEntityMongo struct {
 	Timestamp   int64                           `bson:"timestamp"`
 }
 type AuctionRepository struct {
-	Collection *mongo.Collection
+	Collection      *mongo.Collection
+	auctionInterval time.Duration
 }
 
 func NewAuctionRepository(database *mongo.Database) *AuctionRepository {
-	return &AuctionRepository{
-		Collection: database.Collection("auctions"),
+	ar := &AuctionRepository{
+		Collection:      database.Collection("auctions"),
+		auctionInterval: getAuctionInterval(),
 	}
+
+	go ar.initExpiredAuctionsMonitoring()
+
+	return ar
 }
 
 func (ar *AuctionRepository) CreateAuction(
 	ctx context.Context,
 	auctionEntity *auction_entity.Auction) *internal_error.InternalError {
+
 	auctionEntityMongo := &AuctionEntityMongo{
 		Id:          auctionEntity.Id,
 		ProductName: auctionEntity.ProductName,
@@ -47,4 +58,82 @@ func (ar *AuctionRepository) CreateAuction(
 	}
 
 	return nil
+}
+
+func (ar *AuctionRepository) initExpiredAuctionsMonitoring() {
+	ctx := context.Background()
+	ticker := time.NewTicker(1 * time.Second)
+
+	for range ticker.C {
+
+		activeAuctions := ar.findActiveAuctions(ctx)
+
+		if len(activeAuctions) == 0 {
+			continue
+		}
+
+		expiredAuctions := ar.filterExpiredAuctions(activeAuctions)
+
+		if len(expiredAuctions) == 0 {
+			continue
+		}
+
+		ar.updateAuctionsToStatusCompleted(ctx, expiredAuctions)
+
+	}
+
+}
+
+func (ar *AuctionRepository) filterExpiredAuctions(auctions []auction_entity.Auction) []auction_entity.Auction {
+	expiredAuctions := make([]auction_entity.Auction, 0)
+
+	for _, auction := range auctions {
+
+		auctionEndTime := auction.Timestamp.Add(ar.auctionInterval)
+		auctionExpired := time.Now().After(auctionEndTime)
+
+		if auctionExpired {
+			expiredAuctions = append(expiredAuctions, auction)
+		}
+
+	}
+
+	return expiredAuctions
+}
+
+func (ar *AuctionRepository) findActiveAuctions(ctx context.Context) []auction_entity.Auction {
+	auctions, _ := ar.FindAuctions(ctx, auction_entity.Active, "", "")
+	return auctions
+}
+
+func (ar *AuctionRepository) updateAuctionsToStatusCompleted(ctx context.Context, auctions []auction_entity.Auction) {
+	logger.Info(fmt.Sprintf("Updating %d expired auctions", len(auctions)))
+
+	for _, auction := range auctions {
+
+		_, err := ar.Collection.UpdateOne(
+			ctx,
+			bson.M{"_id": auction.Id},
+			bson.D{
+				{"$set", bson.D{
+					{"status", auction_entity.Completed},
+				}},
+			},
+		)
+
+		if err != nil {
+			logger.Error("Error trying to update auction status to Completed", err)
+		}
+
+	}
+}
+
+func getAuctionInterval() time.Duration {
+	auctionInterval := os.Getenv("AUCTION_INTERVAL")
+	duration, err := time.ParseDuration(auctionInterval)
+	if err != nil {
+		return time.Minute * 5
+	}
+
+	return duration
 }
